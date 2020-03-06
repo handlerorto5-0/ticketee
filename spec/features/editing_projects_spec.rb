06336a849b1fbea8fda3df1d836a999005ac992c
KVexcavator <str_token@mail.ru>
2020-03-06 22:22:34 +0300

    project can now be updated

--- spec/features/editing_projects_spec.rb
+++ spec/features/editing_projects_spec.rb
@@ -1,16 +1,25 @@
 require "rails_helper"
 
 RSpec.feature "Users can edit existing projects" do
-  scenario "with valid attributes" do
+  before do
     FactoryGirl.create(:project, name: "NeoVim")
 
     visit "/"
     click_link "NeoVim"
     click_link "Edit Project"
+  end
+  scenario "with valid attributes" do
     fill_in "Name", with: "NeoVim beta"
     click_button "Update Project"
 
     expect(page).to have_content "Project has been updated."
     expect(page).to have_content "NeoVim beta"
+  end
+
+  scenario "when providing invalid attributes" do
+    fill_in "Name", with: ""
+    click_button "Update Project"
+
+    expect(page).to have_content "Project has not been updated."
   end
 end
