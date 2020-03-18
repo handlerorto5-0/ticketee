2256dab3a742b1c55dd60bc51eb74f318fa9073b
KVexcavator <str_token@mail.ru>
2020-03-18 16:08:51 +0300

    migrate db

--- db/migrate/20200318125746_create_tickets.rb
+++ db/migrate/20200318125746_create_tickets.rb
@@ -3,9 +3,9 @@ class CreateTickets < ActiveRecord::Migration[5.2]
     create_table :tickets do |t|
       t.string :name
       t.text :description
-      t.references :project, foreign_key: true
+      t.references :project, index: true, foreign_key: true
 
-      t.timestamps
+      t.timestamps null: false
     end
   end
 end
