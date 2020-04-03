248480545217b2065fdccff5b7b19b66161a3fdf
KVexcavator <str_token@mail.ru>
2020-04-03 18:27:57 +0300

    Implement deleting tickets feature

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -26,6 +26,12 @@ class TicketsController < ApplicationController
 
   end
 
+  def destroy
+    @ticket.destroy
+    flash[:notice] = "Ticket has been deleted."
+    redirect_to @project
+  end
+
   def update
     if @ticket.update(ticket_params)
       flash[:notice] = "Ticket has been updated."
