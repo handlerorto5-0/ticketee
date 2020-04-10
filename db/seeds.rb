d4e73a01db4ce288ca6f1b470cd75aedf7a75880
KVexcavator <str_token@mail.ru>
2020-04-10 20:16:03 +0300

    Added admin flag to User model, and seeded the first admin user

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,11 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+unless User.exists?(email: "admin@mail.ru")
+  User.create!(email: "admin@mail.ru", password: "password", admin: true)
+end
+unless User.exists?(email: "viewer@mail.ru")
+  User.create!(email: "viewer@mail.ru", password: "password")
+end
+["Sublime Text 3", "Internet Explorer", "NeoVim"].each do |name|
+  unless Project.exists?(name: name)
+    Project.create!(name: name, description: "A sample project about #{name}")
+  end
+end
