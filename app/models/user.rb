a64849e66d4460b20c6ebbc4b117f7647cfe542f
KVexcavator <str_token@mail.ru>
2020-04-22 17:34:13 +0300

    Add the ability to create admin users through the admin backend

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ApplicationRecord
+  def to_s
+    "#{email} (#{admin? ? "Admin" : "User"})"
+  end
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
