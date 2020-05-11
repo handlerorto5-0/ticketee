7084e5631e5bd580a937a145bedc3920a9497fd0
KVexcavator <str_token@mail.ru>
2020-05-11 22:25:49 +0300

    Only members of a projeckt can view tickets in it

--- spec/policies/ticket_policy_spec.rb
+++ spec/policies/ticket_policy_spec.rb
@@ -1,27 +1,45 @@
 require 'rails_helper'
 
 RSpec.describe TicketPolicy, type: :policy do
-  let(:user) { User.new }
+  context " permissions" do
+    subject { TicketPolicy.new(author, ticket) }
 
-  subject { described_class }
+    let(:author) { FactoryGirl.create(:user) }
+    let(:project) { FactoryGirl.create(:project) }
+    let(:ticket) { FactoryGirl.create(:ticket, project: project, author: author) }
 
-  permissions ".scope" do
-    pending "add some examples to (or delete) #{__FILE__}"
+    context "for anonymous users" do
+      let(:user) {nil}
+
+      it {should_not permit_action :show}
+    end
+    context "for viewers of the project" do
+      before {assign_role!(author, :viewer, project)}
+
+      it {should permit_action :show}
+    end
+    context "for editors of the project" do
+      before {assign_role!(author, :editor, project)}
+
+      it {should permit_action :show}
+    end
+    context "for managers of the project" do
+      before {assign_role!(author, :manager, project)}
+
+      it {should permit_action :show}
+    end
+    context "for managers of other projects" do
+      before do
+        assign_role!(author, :manager, FactoryGirl.create(:project))
+      end
+
+      it {should_not permit_action :show}
+    end
+    context "for administrators" do
+      let(:author) {FactoryGirl.create(:user, :admin)}
+
+      it {should permit_action :show}
+    end
   end
 
-  permissions :show? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :create? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :update? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
-
-  permissions :destroy? do
-    pending "add some examples to (or delete) #{__FILE__}"
-  end
 end
