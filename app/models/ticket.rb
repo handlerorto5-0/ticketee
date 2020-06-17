88a1cf9522ac6e03df29eb94e531428310a199bc
KVexcavator <str_token@mail.ru>
2020-06-17 18:56:27 +0300

    Users can now upload 3 files at a time when creating a ticket

--- app/models/ticket.rb
+++ app/models/ticket.rb
@@ -1,10 +1,11 @@
 class Ticket < ApplicationRecord
   belongs_to :project
   belongs_to :author, class_name: "User"
+  has_many :attachments, dependent: :destroy
 
   validates :name, presence: true 
   validates :description, presence: true, length: { minimum: 10  } 
 
   #upload file with carrerwave
-  mount_uploader :attachment, AttachmentUploader
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank
 end
